678d0ace12ecef57f860955c39e0266ce104c4cc
francois-gm <francois.girardm@gmail.com>
2024-12-08 23:29:07 +0100

    commit

--- 2024-2025-go4-thesis-assignment/02a-html-boilerplate/02b-boilerplate-with-css/boilerplate-with-css/assets/js/script.js
+++ 2024-2025-go4-thesis-assignment/02a-html-boilerplate/02b-boilerplate-with-css/boilerplate-with-css/assets/js/script.js
@@ -1,7 +1,26 @@
-// if you use jQuery, you need to start your .js file with this.
-// otherwise you can delete everything here.
-$(document).ready(function() {
 
-// functions go here
+
+
+// we make sure the JavaScript file loads after our HTML by using a function test if the HTML is loaded
+
+function docReady(fn) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+}   
+
+
+
+docReady(function() {
+
+	// functions
+	// go
+	// here
 
 });
+
+
